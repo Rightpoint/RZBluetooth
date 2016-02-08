7ad0ae885ac08a22860acacda159e44b1af32c7c
Brian King <brian.king@raizlabs.com>
2016-02-08 15:35:40 -0500

    Add nullability information to blocks

--- RZBluetooth/RZBDefines.h
+++ RZBluetooth/RZBDefines.h
@@ -16,25 +16,17 @@ typeof(object.p) property __unused; \
 
 #define RZB_OVERLOADED FOUNDATION_EXTERN __attribute__((overloadable))
 
+NS_ASSUME_NONNULL_BEGIN
 
 typedef void(^RZBScanBlock)(CBPeripheral *peripheral, NSDictionary *advInfo, NSNumber *RSSI);
-typedef void(^RZBErrorBlock)(NSError *error);
+typedef void(^RZBErrorBlock)(NSError *__nullable error);
 typedef void(^RZBStateBlock)(CBCentralManagerState state);
 typedef void(^RZBRestorationBlock)(NSArray *peripherals);
-typedef void(^RZBRSSIBlock)(NSNumber *RSSI, NSError *error);
+typedef void(^RZBRSSIBlock)(NSNumber *RSSI, NSError *__nullable error);
 
-typedef void(^RZBPeripheralBlock)(CBPeripheral *peripheral, NSError *error);
-typedef void(^RZBServiceBlock)(CBService *service, NSError *error);
-typedef void(^RZBCharacteristicBlock)(CBCharacteristic *characteristic, NSError *error);
+typedef void(^RZBPeripheralBlock)(CBPeripheral *peripheral, NSError *__nullable error);
+typedef void(^RZBServiceBlock)(CBService *service, NSError *__nullable error);
+typedef void(^RZBCharacteristicBlock)(CBCharacteristic *characteristic, NSError *__nullable error);
 
-@protocol RZBBluetoothRepresentable <NSObject>
+NS_ASSUME_NONNULL_END
 
-+ (CBUUID *)serviceUUID;
-+ (NSDictionary *)characteristicUUIDsByKey;
-+ (CBCharacteristicProperties)characteristicPropertiesForKey:(NSString *)key;
-+ (id)valueForKey:(NSString *)key fromData:(NSData *)data;
-+ (NSData *)dataForKey:(NSString *)key fromValue:(NSString *)value;
-
-- (id)valueForKey:(NSString *)key;
-
-@end
