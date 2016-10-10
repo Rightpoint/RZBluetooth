c2599d778aac5ca063168e8b56d0bdb337e7bb67
Brian King <brian.king@raizlabs.com>
2016-10-10 12:25:56 -0400

    Configure Travis to build 7.3 and 8.0

--- build.sh
+++ build.sh
@@ -1,17 +1,6 @@
-set -o pipefail
-set -e
-
-# "warm" iPhone 6 simulator by pre-launching it so tests don't hang
-export IOS_SIMULATOR_UDID=`instruments -s devices | grep "iPhone 6 (9.3" | awk -F '[\[]' '{print $2}' | awk -F '[\]]' '{print $1}'`
-open -a "simulator" --args -CurrentDeviceUDID $IOS_SIMULATOR_UDID
-
 # Run tests on iPhone 6 simulator (64-bit)
-set -o pipefail && xcodebuild test -project RZBluetooth.xcodeproj -scheme RZBluetooth -destination 'name=iPhone 6' ONLY_ACTIVE_ARCH=NO | xcpretty
-
-# "warm" iPhone 4s simulator
-export IOS_SIMULATOR_UDID=`instruments -s devices | grep "iPhone 4s (9.3" | awk -F '[\[]' '{print $2}' | awk -F '[\]]' '{print $1}'`
-open -a "simulator" --args -CurrentDeviceUDID $IOS_SIMULATOR_UDID
+set -o pipefail && xcodebuild build-for-testing test-without-building -project RZBluetooth.xcodeproj -scheme RZBluetooth -destination 'name=iPhone 6' ONLY_ACTIVE_ARCH=NO | xcpretty
 
 # Run tests on iPhone 4s simulator (32-bit)
-set -o pipefail && xcodebuild test -project RZBluetooth.xcodeproj -scheme RZBluetooth -destination 'name=iPhone 4s' ONLY_ACTIVE_ARCH=NO | xcpretty
+set -o pipefail && xcodebuild clean build-for-testing test-without-building -project RZBluetooth.xcodeproj -scheme RZBluetooth -destination 'name=iPhone 4s' ONLY_ACTIVE_ARCH=NO | xcpretty
 
