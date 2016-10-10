270d80f746e7f8201fdb43ca83c1f23930282c48
Brian King <brian.king@raizlabs.com>
2016-10-10 12:27:08 -0400

    Remove old buld.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-# Run tests on iPhone 6 simulator (64-bit)
-set -o pipefail && xcodebuild build-for-testing test-without-building -project RZBluetooth.xcodeproj -scheme RZBluetooth -destination 'name=iPhone 6' ONLY_ACTIVE_ARCH=NO | xcpretty
-
-# Run tests on iPhone 4s simulator (32-bit)
-set -o pipefail && xcodebuild clean build-for-testing test-without-building -project RZBluetooth.xcodeproj -scheme RZBluetooth -destination 'name=iPhone 4s' ONLY_ACTIVE_ARCH=NO | xcpretty
-
