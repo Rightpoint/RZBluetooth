ca62ef19c288a1bae4ffb3635fad3ed8bc158cb8
Brian King <brian.king@raizlabs.com>
2016-11-22 16:55:30 -0500

    Update Podfile

--- RZBluetoothExample/Pods/Headers/Private/RZBluetooth/RZBMockedCentralManager.h
+++
@@ -1,1 +0,0 @@
-../../../../../RZMockBluetooth/Mock/RZBMockedCentralManager.h
---
+++ RZBluetoothExample/Pods/Target Support Files/RZBluetooth-Core-Mock-Test/RZBluetooth-Core-Mock-Test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RZBluetooth_Core_Mock_Test : NSObject
+@end
+@implementation PodsDummy_RZBluetooth_Core_Mock_Test
+@end
